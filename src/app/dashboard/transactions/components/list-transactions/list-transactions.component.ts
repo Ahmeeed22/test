d476481fbcc95988967c67a4cfcf4b88023de0c6
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2023-05-01 21:35:23 +0400

    fix bugs

--- src/app/dashboard/transactions/components/list-transactions/list-transactions.component.ts
+++ src/app/dashboard/transactions/components/list-transactions/list-transactions.component.ts
@@ -33,6 +33,7 @@ export class ListTransactionsComponent implements OnInit {
   @ViewChild('immg') immg !: ElementRef;
   @ViewChild('infoo') infoo !: ElementRef;
   @ViewChild('footerInvoice') footerInvoice !: ElementRef;
+  @ViewChild('footerInvoice2') footerInvoice2 !: ElementRef;
 // pagination setup
   length = 50;
   pageSize = 10;
@@ -103,6 +104,7 @@ export class ListTransactionsComponent implements OnInit {
       this.spinnerService.show()
       this.immg.nativeElement.classList.toggle('d-block');
       this.footerInvoice.nativeElement.classList.toggle('d-none');
+      this.footerInvoice2.nativeElement.classList.toggle('d-none');
       this.infoo.nativeElement.classList.toggle('d-block');
       setTimeout(() => {
         es6printJS(x);
@@ -113,6 +115,7 @@ export class ListTransactionsComponent implements OnInit {
         this.immg.nativeElement.classList.toggle('d-block');
         this.infoo.nativeElement.classList.toggle('d-block');
         this.footerInvoice.nativeElement.classList.toggle('d-none');
+        this.footerInvoice2.nativeElement.classList.toggle('d-none');
         this.spinnerService.hide()
       }, 400);
     }
